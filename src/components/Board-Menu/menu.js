d4f45af64013fe2e1a1db617ed8b8ad865121e25
Yinka <yinka1911@gmail.com>
2019-01-18 16:41:38 -0500

    still working on lightbox. Footer almost done but updated.

--- src/components/Board-Menu/menu.js
+++ src/components/Board-Menu/menu.js
@@ -15,7 +15,6 @@ class MenuX extends React.Component {
 
   render() {
     const { selectedOption } = this.state
-
     const data = this.props.menu.allContentfulBoardMenu.edges[0].node
       .menuForBoards
 
--- src/components/Chart/chart.js
+++ src/components/Chart/chart.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { StaticQuery, graphql } from 'gatsby'
 import { Accordion, AccordionItem } from 'react-sanfona'
-
+import Lightbox from 'react-image-lightbox'
+import 'react-image-lightbox/style.css' // This only needs to be imported once in your app
 import {
   TabContent,
   TabPane,
@@ -18,6 +19,13 @@ import {
 import classnames from 'classnames'
 import charts from './chart.module.css'
 import './chart-accordion.css'
+
+const images = [
+  '//placekitten.com/1500/500',
+  '//placekitten.com/4000/3000',
+  '//placekitten.com/800/1200',
+  '//placekitten.com/1500/1500',
+]
 
 class Chart extends React.Component {
   /*   <Accordion>
@@ -38,12 +46,15 @@ class Chart extends React.Component {
       )
     })}
   </Accordion> */
+
   constructor(props) {
     super(props)
 
     this.toggle = this.toggle.bind(this)
     this.state = {
       activeTab: '1',
+      photoIndex: 0,
+      isOpen: false,
     }
   }
 
@@ -54,14 +65,49 @@ class Chart extends React.Component {
       })
     }
   }
+
   render() {
     /* const data = this.props.acc.allFile.edges */
     const barrie = this.props.acc.Barrie.edges
     const toronto = this.props.acc.Toronto.edges
     const victoria = this.props.acc.Victoria.edges
 
+    const { photoIndex, isOpen } = this.state
+
+    console.log('barrie', barrie)
+    const bx = barrie.map((xxy, id) => {
+      console.log('bx', xxy.node.childImageSharp.fluid.src)
+      return (
+        <div>
+          <img src={xxy.node.childImageSharp.fluid.src} />
+        </div>
+      )
+    })
+    console.log('bmx', bx)
     return (
       <div>
+        <button type="button" onClick={() => this.setState({ isOpen: true })}>
+          Open Lightbox
+        </button>
+
+        {isOpen && (
+          <Lightbox
+            mainSrc={bx[photoIndex]}
+            nextSrc={bx[(photoIndex + 1) % bx.length]}
+            prevSrc={bx[(photoIndex + bx.length - 1) % bx.length]}
+            onCloseRequest={() => this.setState({ isOpen: false })}
+            onMovePrevRequest={() =>
+              this.setState({
+                photoIndex: (photoIndex + bx.length - 1) % bx.length,
+              })
+            }
+            onMoveNextRequest={() =>
+              this.setState({
+                photoIndex: (photoIndex + 1) % bx.length,
+              })
+            }
+          />
+        )}
         <Nav tabs className={charts.tabTitle}>
           <NavItem>
             <NavLink
@@ -93,6 +139,76 @@ class Chart extends React.Component {
               GREATER VANCOUVER
             </NavLink>
           </NavItem>
+          <NavItem>
+            <NavLink
+              className={classnames({ active: this.state.activeTab === '4' })}
+              onClick={() => {
+                this.toggle('4')
+              }}
+            >
+              REGINA
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              className={classnames({ active: this.state.activeTab === '5' })}
+              onClick={() => {
+                this.toggle('5')
+              }}
+            >
+              BARRIE
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              className={classnames({ active: this.state.activeTab === '6' })}
+              onClick={() => {
+                this.toggle('6')
+              }}
+            >
+              FRASER VALLEY
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              className={classnames({ active: this.state.activeTab === '7' })}
+              onClick={() => {
+                this.toggle('7')
+              }}
+            >
+              SASKATOON
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              className={classnames({ active: this.state.activeTab === '8' })}
+              onClick={() => {
+                this.toggle('8')
+              }}
+            >
+              OTTAWA
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              className={classnames({ active: this.state.activeTab === '9' })}
+              onClick={() => {
+                this.toggle('9')
+              }}
+            >
+              GUELPH
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              className={classnames({ active: this.state.activeTab === '10' })}
+              onClick={() => {
+                this.toggle('10')
+              }}
+            >
+              NIAGRA REGION
+            </NavLink>
+          </NavItem>
         </Nav>
         <TabContent
           activeTab={this.state.activeTab}
@@ -100,11 +216,11 @@ class Chart extends React.Component {
         >
           <TabPane tabId="1">
             <Row>
-              {/*     {barrie.map((item, id) => (
+              {barrie.map((item, id) => (
                 <Col sm="3" key={id}>
                   <img src={item.node.childImageSharp.fluid.src} />
                 </Col>
-              ))} */}
+              ))}
             </Row>
           </TabPane>
           <TabPane tabId="2">
