5f55a952ec3b50d17ad5c9b4c52f8b0651d728f7
Yinka <yinka1911@gmail.com>
2019-03-14 20:26:31 -0400

    forgot to update components

--- src/components/Mobile-menu/burgerIcon.js
+++ src/components/Mobile-menu/burgerIcon.js
@@ -5,5 +5,6 @@ export default ({ open, ...props }) => (
     <div className="bar1" key="b1" />
     <div className="bar2" key="b2" />
     <div className="bar3" key="b3" />
+    <div className="bar3" key="b3" />
   </div>
 )
