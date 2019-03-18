223e7bd68fef0e74945627abb31be898d647d444
yinka <yinka1911@gmail.com>
2019-03-18 14:47:04 -0400

    Update boards.js

--- src/templates/boards.js
+++ src/templates/boards.js
@@ -37,6 +37,14 @@ class BoardTemplate extends React.Component {
           <div className="row split">
             <div className="col-md">
               <div className="entry-header">
+      <div>
+                          <p
+                            className="content"
+                            dangerouslySetInnerHTML={{
+                              __html: post.wtf.childContentfulRichText.html,
+                            }}
+                          />
+                        </div>
                 <h1
                   className="entry-title"
                   dangerouslySetInnerHTML={{
